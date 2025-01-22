4d39e179c9751c91a177f464994a7af97dd820f4
ycggyao <yao18274936495@163.com>
2025-01-22 19:45:56 +0800

    fix(backend): redis 补充整机替换亲和性 #9192 # Reviewed, transaction id: 29813

--- dbm-ui/backend/ticket/builders/redis/redis_toolbox_add_slave.py
+++ dbm-ui/backend/ticket/builders/redis/redis_toolbox_add_slave.py
@@ -8,9 +8,12 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
+import itertools
+
 from django.utils.translation import ugettext_lazy as _
 from rest_framework import serializers
 
+from backend.configuration.constants import AffinityEnum
 from backend.db_meta.enums import InstanceRole, InstanceStatus
 from backend.db_meta.models import Cluster, Machine, StorageInstanceTuple
 from backend.db_services.dbbase.constants import IpSource
@@ -86,16 +89,33 @@ class RedisAddSlaveFlowBuilder(BaseRedisTicketFlowBuilder):
                 bk_host_id__in=master_hosts
             )
         }
+        cluster_ids = list(itertools.chain(*[infos["cluster_ids"] for infos in self.ticket.details["infos"]]))
+        id__cluster = {cluster.id: cluster for cluster in Cluster.objects.filter(id__in=cluster_ids)}
+
         for info in self.ticket.details["infos"]:
             info["resource_spec"] = {}
+            # 取第一个cluster即可，即使是多集群，也是单机多实例的情况
+            cluster = id__cluster[info["cluster_ids"][0]]
             for pair in info["pairs"]:
                 # 申请的 new slave, 需要和当前集群中的 master 不同机房;
                 master_machine = id__machine[pair["redis_master"]["bk_host_id"]]
                 pair["redis_slave"]["location_spec"] = {
                     "city": master_machine.bk_city.logical_city.name,
-                    "sub_zone_ids": [master_machine.bk_sub_zone_id],
-                    "include_or_exclue": False,
+                    "sub_zone_ids": [],
                 }
+                pair["redis_slave"].update(affinity=cluster.disaster_tolerance_level)
+                if cluster.disaster_tolerance_level == AffinityEnum.CROS_SUBZONE:
+                    pair["redis_slave"]["location_spec"].update(
+                        sub_zone_ids=[master_machine.bk_sub_zone_id], include_or_exclue=False
+                    )
+
+                elif cluster.disaster_tolerance_level in [
+                    AffinityEnum.SAME_SUBZONE,
+                    AffinityEnum.SAME_SUBZONE_CROSS_SWTICH,
+                ]:
+                    pair["redis_slave"]["location_spec"].update(
+                        sub_zone_ids=[master_machine.bk_sub_zone_id], include_or_exclue=True
+                    )
                 info["resource_spec"][pair["redis_master"]["ip"]] = pair["redis_slave"]
 
         self.ticket.save(update_fields=["details"])
